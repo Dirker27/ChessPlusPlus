507066ed8e6c8bcbf5582242d5af644b080677b6
Dirker27 <hortensd@gmail.com>
2015-10-21 10:04:28 -0700

    Skeleton Main Stub and Makefile

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <getopt.h>
+
+bool g_verbose = false;
+bool g_echo = false;
+
+int main(int argc, char** argv) {
+
+	//- Argument Extraction ------------------------------=
+	//
+	int opt;
+	while ((opt = getopt(argc, argv, "ve")) != -1) {
+		switch(opt) {
+			case 'v':
+				printf("\\> [Verbose Mode]\n");
+				g_verbose = true;
+				break;
+			case 'e':
+				printf("\\> [Echo Mode]\n");
+				g_echo = true;
+				break;
+			default:
+				// You're drunk. Get help.
+				printf("Usage: -p [port] | -r [relayhost:port] | -R [path]\n");
+				return EXIT_SUCCESS;
+		}
+	}
+
+	// TODO: Launch Game
+
+	return EXIT_SUCCESS;
+}
+
