ef2ccb5b551f65e5155df999f02e240580de3197
Dirker27 <hortensd@gmail.com>
2015-10-25 11:04:15 -0700

    I don't even know anymore

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,12 +1,26 @@
+#include <stdlib.h>
 #include <stdio.h> // TODO [C++11] - to I/O Stream
 
 #include "logger.h"
 
+static CustomSystemLogger* logger = logger->initialize();
+static char* write_string = (char*) malloc(sizeof(char) * 256);
+
+void log_memory(const char* message) {
+	logger->log_heap_allocation(message);
+}
+
 void log_memory(const char* message, bool allocated) {
-	const char* allocd = (allocated) ? "[MEM-Allocation]" : "[MEM-Deallocation]";
-	if (g_log_memory) {
-		printf("%s %s\n", allocd, message);
-	}
+	log_memory(message, allocated, NULL);
+}
+
+void log_memory(const char* message, bool allocated, char* appendDecoration) {
+	logger->appendDecoration(appendDecoration);
+	const char* allocd = (allocated) ? "[MEM - Allocation]" : "[MEM-Deallocation]";
+
+	for (int i=0;i<256;i++) { write_string[i] = '\0'; }
+	sprintf(write_string, "%s %s\n", allocd, message);
+	logger->log_heap_allocation(write_string);
 }
 
 void log_action(const char* message) {
--- src/logger.h
+++ src/logger.h
@@ -1,12 +1,71 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <stdbool.h>
 
+static const int MAX_DECORATION_LENGTH = 12;
+
+// Settable from CLI switches
 extern bool g_log_memory;
 extern bool g_log_game;
 
+/**
+ * Logger Utility
+ * 
+ * Accessed from proxy methods.
+ */
+class CustomSystemLogger {
+	private:
+		static CustomSystemLogger* logger;
+
+		static int decorationLength;
+		static char* decoration;
+
+	public:
+		CustomSystemLogger(void) {
+			decorationLength = 0;
+			decoration = (char*) malloc(sizeof(char) * 128);
+		}
+		~CustomSystemLogger(void) {
+			shutdown();
+		}
+
+		static CustomSystemLogger* initialize(void) {
+			if (logger == NULL) {
+				logger = new CustomSystemLogger();
+			}
+			return logger;
+		};
+		static void shutdown(void) {
+			delete(logger);
+			logger = NULL;
+		};
+
+		//- Operations -----------------------------------=
+		//
+		void log_heap_allocation(const char* message) {
+			printf("%s\n", message);
+		};
+		void log_action(const char* message) {
+			printf("%s\n", message);
+		};
+
+		//- Decoration -----------------------------------=
+		//
+		void appendDecoration(char* dec) {
+			if (logger->decorationLength < MAX_DECORATION_LENGTH) {
+				sprintf(decoration, "%s%s", decoration, dec);
+				logger->decorationLength++;
+			}
+		};
+		static char* getDecoration(void) { return decoration; };
+};
+
+void log_memory(const char*);
 void log_memory(const char*, bool);
+void log_memory(const char*, bool, char*);
 void log_action(const char*);
 
 #endif
